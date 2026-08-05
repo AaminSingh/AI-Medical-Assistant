e520b0f498221afdc88181ea2f9b3f63d9d8ac4e
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2026-08-05 17:13:19 +0530

    updating the file

--- AI_medical_backend MDB/src/controllers/auth.controller.js
+++ AI_medical_backend MDB/src/controllers/auth.controller.js
@@ -8,7 +8,7 @@ import { asyncHandler } from "../utils/async-handler.js";
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "lax",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   maxAge: 24 * 60 * 60 * 1000, // 1 day
 };
 
